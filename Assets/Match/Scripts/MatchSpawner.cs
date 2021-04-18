956c8316c584a9a2cee148b28b018b2131e4384b
Alec John <gonzalesalec@gmail.com>
2021-04-18 12:45:50 +0800

    Create Match game

---
+++ Assets/Match/Scripts/MatchSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Spawning.Scripts;
+using UnityEngine;
+
+namespace Match.Scripts {
+    public class MatchSpawner : Spawner {
+
+        public List<Transform> anchors;
+
+        protected override void Start() {
+            chanceOfSpawningBad = 0f;
+            Respawn();
+        }
+
+        public void Respawn() {
+            Clear();
+
+            // Randomize position of the
+            // two matching character
+            int pos1, pos2;
+            pos1 = Random.Range(
+                0, anchors.Count);
+            do {
+                pos2 = Random.Range(
+                    0, anchors.Count);
+            } while (pos2 == pos1);
+
+            // Generate the item to match
+            var matchingItem = GetRandomItem();
+
+            // Spawn an item for every anchor
+            foreach (var t in anchors) {
+                SpawnItem item; do {
+                    item = GetRandomItem();
+                } while (spawned.Select(x =>
+                    x.item).Contains(matchingItem));
+                Spawn(item, t.position);
+            }
+        }
+
+    }
+}
