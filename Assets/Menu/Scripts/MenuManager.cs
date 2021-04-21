57bbd3cce3db1b2309f5e423943eb67fe99be1ff
Alec John <gonzalesalec@gmail.com>
2021-04-21 22:52:10 +0800

    Always show Survey Panel on app start

--- Assets/Menu/Scripts/MenuManager.cs
+++ Assets/Menu/Scripts/MenuManager.cs
@@ -3,18 +3,12 @@ using UnityEngine;
 namespace Menu.Scripts {
     public class MenuManager : MonoBehaviour {
 
-        private bool _isSurveyShowed;
-        private bool _shouldShowSurvey;
+        private static bool _isSurveyShowed;
         public GameObject menu;
         public GameObject survey;
 
-        private void Start() {
-           _shouldShowSurvey = (!PlayerPrefs.HasKey("SurveyAnswered") ||
-                                PlayerPrefs.GetInt("SurveyAnswered") != 1);
-        }
-
         private void Update() {
-            if (_shouldShowSurvey && !_isSurveyShowed) {
+            if (!_isSurveyShowed) {
                 menu.SetActive(false);
                 survey.SetActive(true);
                 _isSurveyShowed = true;
